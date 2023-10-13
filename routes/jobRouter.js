85de3c53ce023862ef966c71b19ceead5cc347cf
Simon Mac Aree <smacr25@gmail.com>
2023-10-13 15:14:19 +0100

    Creating controller and route folders with files
    Moving job api's to controller folder. Creating route folder
    Refactoring the server js file and moving code to above folders.

---
+++ routes/jobRouter.js
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+
+const router = Router();
+
+import {getAllJobs, getJob, createJob, updateJob, deleteJob} from '../controllers/jobControllers.js';
+
+router.route('/').get(getAllJobs).post(createJob); 
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob);
+
+export default router;
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ import express from 'express'
 const app = express()
 
 import morgan from 'morgan';
-import { nanoid } from 'nanoid';
 
+// routers
+import jobRouter from './routes/jobRouter.js';
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
@@ -22,74 +23,8 @@ if(process.env.NODE_ENV === 'development') {
      res.json({message: 'Data received', data:req.body});
  })
 
-let jobs = [
-    { id: nanoid(), company: 'apple', position: 'front-end' },
-    { id: nanoid(), company: 'google', position: 'back-end' },
-  ];
 
-// Get all jobs
-app.get('/api/v1/jobs', (req, res) => {
-    res.status(200).json({jobs});
-})
-
-// create job
-app.post('/api/v1/jobs', (req, res) => {
-    const { company, position } = req.body;
-    if (!company || !position) {
-      return res.status(400).json({ msg: 'please provide company and position' });
-    }
-    const id = nanoid(10);
-    // console.log(id);
-    const job = { id, company, position };
-    jobs.push(job);
-    res.status(201).json({ job });
-  });
-
-// Get single job
-app.get('/api/v1/jobs/:id', (req, res) => {
-    const {id} = req.params;
-    const job = jobs.find((job) => job.id === id);
-
-    if(!job) {
-        return res.status(404).json({msg: `no job with id ${id}`});
-    }
-
-    res.status(200).json({job});
-});
-
-// Edit job
-app.patch('/api/v1/jobs/:id', (req, res) => {
-    const {company, position} = req.body;
-
-    if(!company || !position) {
-        return res.status(400).json({msg: 'please provide company and position'});
-    }
-
-    const {id} = req.params;
-    const job = jobs.find((job) => job.id === id);
-
-    if (!job) {
-        return res.status(404).json({msg: `no job with id ${id}`});
-    }
-
-    job.company = company;
-    job.position = position;
-    res.status(200).json({msg: 'job modified', job});
-});
-
-// Delete job
-app.delete('/api/v1/jobs/:id', (req, res) => {
-    const { id } = req.params;
-    const job = jobs.find((job) => job.id === id);
-    if (!job) {
-      return res.status(404).json({ msg: `no job with id ${id}` });
-    }
-    const newJobs = jobs.filter((job) => job.id !== id);
-    jobs = newJobs;
-  
-    res.status(200).json({ msg: 'job deleted' });
-  });
-
+app.use('/api/v1/jobs', jobRouter);
 
 app.use('*', (req, res)=> {
     res.status(404).json({msg: 'not found'});
