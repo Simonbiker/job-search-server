f163f4ff2ceff777d33e9be0ce82533412631bd3
Simon Mac Aree <smacr25@gmail.com>
2023-10-13 15:58:25 +0100

    Refactoring and adding express async error package

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,3 +1,5 @@
+import Job from "../models/jobModels.js";
+
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -11,14 +13,12 @@ export const getAllJobs = async (req, res) => {
 
 export const createJob = async (req, res) => {
     const { company, position } = req.body;
-  
-    if (!company || !position) {
-      return res.status(400).json({ msg: 'please provide company and position' });
+    try {
+        const job = await Job.create('something');
+        res.status(201).json({ job });
+    } catch (error) {
+        res.status(500).json({ msg: 'server error' });
     }
-    const id = nanoid(10);
-    const job = { id, company, position };
-    jobs.push(job);
-    res.status(201).json({ job });
   };
 
 export const getJob = async (req, res) => {
