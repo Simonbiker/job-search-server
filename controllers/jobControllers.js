d0eced6ce7e718a2894b3aaefee0ec443cacfea9
Simon Mac Aree <smacr25@gmail.com>
2023-10-16 10:56:35 +0100

    updating deleteJob and updateJob

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -29,33 +29,27 @@ export const getJob = async (req, res) => {
 };
 
 export const updateJob = async (req, res) => {
-    const {company, position} = req.body;
-
-    if(!company || !position) {
-        return res.status(400).json({msg: 'please provide company and position'});
-    }
-
     const {id} = req.params;
-    const job = jobs.find((job) => job.id === id);
+    const updateJob = await Job.findByIdAndUpdate(id, req.body, {
+        new:true
+    });
 
-    if (!job) {
+    if (!updateJob) {
         return res.status(404).json({msg: `no job with id ${id}`});
     }
 
-    job.company = company;
-    job.position = position;
-    res.status(200).json({msg: 'job modified', job});
+    res.status(200).json({msg: 'job modified', job: updateJob});
 };
 
 export const deleteJob = async (req, res) => {
     const { id } = req.params;
-    const job = jobs.find((job) => job.id === id);
-    if (!job) {
+    const removeJob = await Job.findByIdAndDelete(id);
+
+    if (!removeJob) {
       return res.status(404).json({ msg: `no job with id ${id}` });
     }
-    const newJobs = jobs.filter((job) => job.id !== id);
-    jobs = newJobs;
+    
   
-    res.status(200).json({ msg: 'job deleted' });
+    res.status(200).json({ msg: 'job deleted', job: removeJob });
 };
 
