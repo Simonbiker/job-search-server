5595c199ac32a35ae5cffacfda4259a4a8595207
Simon Mac Aree <smacr25@gmail.com>
2023-10-16 15:12:42 +0100

    Adding new function for to validate id

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -3,13 +3,13 @@ import {Router} from 'express';
 const router = Router();
 
 import {getAllJobs, getJob, createJob, updateJob, deleteJob} from '../controllers/jobControllers.js';
-import { validateJobInput } from '../middleware/validationMiddleware.js';
+import { validateJobInput, validateIdParam } from '../middleware/validationMiddleware.js';
 
 router.route('/').get(getAllJobs).post(validateJobInput, createJob);
 router
   .route('/:id')
-  .get(getJob)
-  .patch(validateJobInput, updateJob)
+  .get(validateIdParam, getJob)
+  .patch(validateJobInput, validateIdParam, updateJob)
   .delete(deleteJob);
 
 export default router;
