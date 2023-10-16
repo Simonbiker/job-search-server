65f5f1cbee09f36b5ae3d7e91fe5261f797490aa
Simon Mac Aree <smacr25@gmail.com>
2023-10-16 10:47:19 +0100

    Updating jobControllers.js

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -8,22 +8,18 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({})
     res.status(200).json({jobs});
 };
 
 export const createJob = async (req, res) => {
-    const { company, position } = req.body;
-    try {
-        const job = await Job.create('something');
-        res.status(201).json({ job });
-    } catch (error) {
-        res.status(500).json({ msg: 'server error' });
-    }
+    const job = await Job.create(req.body);
+    res.status(201).json({ job });
   };
 
 export const getJob = async (req, res) => {
     const {id} = req.params;
-    const job = jobs.find((job) => job.id === id);
+    const job = await Job.findById(id);
 
     if(!job) {
         return res.status(404).json({msg: `no job with id ${id}`});
